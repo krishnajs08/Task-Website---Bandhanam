5c898c244fbbbe238f5a3b49ac83e5850cf1a942
Krishna Pangarkar <146456229+krishnajs08@users.noreply.github.com>
2025-10-01 17:51:54 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,3 @@
-// server.js
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
@@ -6,28 +5,18 @@ require("dotenv").config();
 const userRoutes = require("./routes/userRoutes");
 const connectDB = require("./config/ds");
 
-// Connect to database
+
 connectDB();
 
 const app = express();
-
-// Middlewares
 app.use(cors());
 app.use(express.json());
 
-// Routes
+
 app.use("/api/users", userRoutes);
 
-// Root route
-app.get('/', (req, res) => {
-  res.send({
-    activeStatus: true,
-    error: false,
-  });
-});
-
-// Start server
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
